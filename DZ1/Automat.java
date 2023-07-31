bc4069f227b386d512f48055936b9abf0955d818
Alexandr <sasha-bragin@mail.ru>
2023-07-31 19:35:25 +0300

    DZ1

---
+++ DZ1/Automat.java
@@ -0,0 +1,25 @@
+
+package Homework.Homework1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Automat {
+
+  List<Product> listProduct = new ArrayList<>();
+
+  public void initProduct(List<Product> myList) {
+    listProduct = myList;
+  }
+
+  public Product getProduct(String name) {
+    for (Product el : listProduct) {
+      if (el.getName().equals(name)) {
+        return el;
+      }
+    }
+    return null;
+
+  }
+
+}
